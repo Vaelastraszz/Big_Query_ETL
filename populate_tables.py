218edf470e799d45e3007970f0f9388fb0f896d2
Vaelastraszz <romain.lejeun@gmail.com>
2024-06-28 15:50:35 +0200

    adding function to populate customers table
    - if_table_is_empty added to check if table is empty for tests
    - populate customers table working correctly to populate 100k fake
    customers

--- populate_tables.py
+++ populate_tables.py
@@ -12,6 +12,7 @@ def check_connection(**kwargs) -> bool:
         connection = mysql.connector.connect(**kwargs)
         if connection.is_connected():
             print("Connection established")
+            connection.close()
         else:
             print("Connection failed")
     except Error as e:
@@ -19,13 +20,58 @@ def check_connection(**kwargs) -> bool:
         return False
 
 
-def populate_customers(n_customers: int, **kwargs) -> None:
-    pass
+def if_table_is_empty(table_name: str, **kwargs) -> bool:
+    with mysql.connector.connect(**kwargs) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(f"select exists(select 1 from {table_name} limit 1)")
+            result = cursor.fetchone()
+            if result[0] == 0:
+                return True
+            else:
+                return False
+
+
+def populate_customers(
+    n_customers: int, fake: Faker, batch_size: int = 5000, **kwargs
+) -> None:
+
+    if if_table_is_empty("customers", **kwargs):
+
+        insert_query = """
+        INSERT INTO customers (name, date_birth, email_address, phone_number, country)
+        VALUES (%s, %s, %s, %s, %s)"""
+
+        try:
+            with mysql.connector.connect(**kwargs) as connection:
+                with connection.cursor() as cursor:
+                    data = []
+                    for _ in range(n_customers):
+                        name = fake.name()
+                        data_birth = fake.date_of_birth(minimum_age=18, maximum_age=65)
+                        email = fake.email()
+                        phone = fake.phone_number()[:20]
+                        country = fake.country()
+                        data.append((name, data_birth, email, phone, country))
+
+                        if len(data) == batch_size:
+                            cursor.executemany(insert_query, data)
+                            connection.commit()
+                            data = []
+                    if data:
+                        cursor.executemany(insert_query, data)
+                        connection.commit()
+
+        except Error as e:
+            print(f"Error: {e}")
+
+    else:
+        print("Table customers is already populated")
 
 
 if __name__ == "__main__":
 
     load_dotenv()
+    fake = Faker()
 
     my_sql_data = {
         "host": "localhost",
@@ -34,4 +80,4 @@ if __name__ == "__main__":
         "database": "OMNI_MANAGEMENT",
     }
 
-    check_connection(**my_sql_data)
+    populate_customers(100000, fake, **my_sql_data)
