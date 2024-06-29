816568ccccda8d82532a02bbdb5f4635a53d6d6a
Vaelastraszz <romain.lejeun@gmail.com>
2024-06-29 11:35:09 +0200

    function populate purchaseHistory
    -function to add purchasing history in omni_management
    -table successfully populated calling it in main
    -adding also a function to generate discount with probabilities

--- populate_tables.py
+++ populate_tables.py
@@ -1,4 +1,5 @@
 from faker import Faker
+import random
 import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
@@ -144,6 +145,57 @@ def populate_channels(**kwargs) -> None:
         print("Table channels is already populated")
 
 
+def populate_purshaseHistory(n_orders: int, batch_size: int = 10000, **kwargs) -> None:
+
+    def generate_discount() -> float:
+        discount = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6]
+        weights = [0.5, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.025, 0.025, 0.025]
+        return random.choices(discount, weights=weights)[0]
+
+    if if_table_is_empty("purchaseHistory", **kwargs):
+        insert_query = """
+        INSERT INTO purchaseHistory (customer_id, product_sku, channel_id, order_date, quantity, discount)
+        VALUES (%s, %s, %s, %s, %s, %s)"""
+
+        try:
+            with mysql.connector.connect(**kwargs) as connection:
+                with connection.cursor() as cursor:
+                    data = []
+                    for _ in range(n_orders):
+                        customer_id = fake.random_int(min=1, max=1000)
+                        product_id = fake.random_int(min=1, max=3000)
+                        channel_id = fake.random_int(min=1, max=18)
+                        purchase_date = fake.date_time_between(
+                            start_date="-2y", end_date="now"
+                        )
+                        quantity = fake.random_int(min=1, max=10)
+                        discount = generate_discount()
+                        data.append(
+                            (
+                                customer_id,
+                                product_id,
+                                channel_id,
+                                purchase_date,
+                                quantity,
+                                discount,
+                            )
+                        )
+
+                        if len(data) == batch_size:
+                            cursor.executemany(insert_query, data)
+                            connection.commit()
+                            data = []
+                    if data:
+                        cursor.executemany(insert_query, data)
+                        connection.commit()
+
+        except Error as e:
+            print(f"Error: {e}")
+
+    else:
+        print("Table purchaseHistory is already populated")
+
+
 if __name__ == "__main__":
 
     load_dotenv()
@@ -159,3 +211,4 @@ if __name__ == "__main__":
     populate_customers(1000, fake, **my_sql_data)
     populate_products(3000, fake, **my_sql_data)
     populate_channels(**my_sql_data)
+    populate_purshaseHistory(500000, **my_sql_data)
